c17984a43e6fdde3f18676dcb4ca5aa7165ea5b6
byznryldrm <byznryldrm2@gmail.com>
2021-05-08 03:16:18 +0300

    Adminde Yüklenen Resmin Gösterilmesi

--- car/models.py
+++ car/models.py
@@ -24,15 +24,22 @@ class Car(models.Model):
         ('False', 'Hayır'),
     )
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
-    amount = models.IntegerField()
     detail = models.TextField()
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+
+class Images(models.Model):
+    car =models.ForeignKey(Car, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return self.title
+
