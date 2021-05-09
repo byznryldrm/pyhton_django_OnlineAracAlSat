5f320880a4e0002ba5bf2f23c34845d020012dc5
byznryldrm <byznryldrm2@gmail.com>
2021-05-09 15:53:22 +0300

    CKEditör Zengin Metin Editörü Kullanma

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from ckeditor_uploader.fields import RichTextUploadingField
+
 # Create your models here.
 class Setting(models.Model):
     STATUS = (
@@ -22,9 +24,9 @@ class Setting(models.Model):
     facebook = models.CharField(blank=True, max_length=50)
     twitter = models.CharField(blank=True, max_length=50)
     instagram = models.CharField(blank=True, max_length=50)
-    aboutus = models.TextField()
-    contact = models.TextField()
-    references = models.TextField()
+    aboutus = RichTextUploadingField()
+    contact = RichTextUploadingField()
+    references = RichTextUploadingField()
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
