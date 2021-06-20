9390e510eb916f4798c139651b3b57588610f753
Beyza Nur Yıldırım <byznryldrm2@gmail.com>
2021-06-20 15:31:54 +0300

    Kategori ve Urun için otomatik slag oluşturma

--- car/models.py
+++ car/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from mptt.admin import MPTTModelAdmin
@@ -17,7 +18,7 @@ class Category(MPTTModel):
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False, unique=True)
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -37,6 +38,9 @@ class Category(MPTTModel):
         return mark_safe('<image src = "{}" height = "50" />' .format(self.image.url))
     image_tag.short_description = 'Image'
 
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug': self.slug})
+
 
 class Car(models.Model):
     STATUS = (
@@ -50,7 +54,7 @@ class Car(models.Model):
     image = models.ImageField(blank=True, upload_to='images/')
     price = models.FloatField()
     detail = RichTextUploadingField()
-    slug = models.SlugField(blank=True, max_length=100)
+    slug = models.SlugField(null=False, unique=True)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -62,6 +66,8 @@ class Car(models.Model):
         return mark_safe('<image src = "{}" height = "50" />' .format(self.image.url))
     image_tag.short_description = 'Image'
 
+    def get_absolute_url(self):
+        return reverse('car_detail', kwargs={'slug': self.slug})
 
 class Images(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
